70adcf9509945b3fcdb86ad15fd59b09957d8e30
David24 <dkeci21@epoka.edu.al>
2025-10-27 16:10:46 +0100

    Creating the template and the theme of the website

---
+++ website/src/components/Background/SiteBackground.tsx
@@ -0,0 +1,17 @@
+// components/Background/SiteBackground.tsx
+'use client'
+
+import Grid from './Grid'
+import Blurs from './Blurs'
+
+export default function SiteBackground() {
+  return (
+    <div className="pointer-events-none fixed inset-0 -z-50 overflow-hidden">
+      <div className="absolute inset-0 bg-[#ededed] dark:bg-black" />
+      <div className="absolute inset-0">
+        <Grid />
+        <Blurs />
+      </div>
+    </div>
+  )
+}
