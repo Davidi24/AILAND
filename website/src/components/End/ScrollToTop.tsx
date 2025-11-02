e966351209ddff36a33ac1a34fe88691a0d18faf
RanaQasim <ranaqasim739@yahoo.com>
2025-11-02 20:36:29 +0100

    adding scrollToTop button

---
+++ website/src/components/End/ScrollToTop.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    }
+
+    window.addEventListener('scroll', toggleVisibility)
+    return () => window.removeEventListener('scroll', toggleVisibility)
+  }, [])
+
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  
