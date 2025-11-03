63b7f53abb585ab88727e7d025d9c7c7a1616357
David24 <dkeci21@epoka.edu.al>
2025-11-03 19:56:15 +0100

    test

--- website/src/app/dynamicImport.tsx
+++ website/src/app/dynamicImport.tsx
@@ -8,6 +8,10 @@ export default function ThreeGraphBG(props: any) {
   const [show, setShow] = useState(false)
 
   useEffect(() => {
+    // detect iPhone or iPad or Android
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+    if (isMobile) return // do NOT render the 3D graph
+
     const id = requestIdleCallback(() => setShow(true))
     return () => cancelIdleCallback(id)
   }, [])
