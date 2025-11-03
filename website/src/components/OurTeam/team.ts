489a184b910467cdd8f531c889beb43c576a62d1
era-e <your-email@example.com>
2025-11-03 17:17:24 +0100

    profile pic

--- website/src/components/OurTeam/team.ts
+++ website/src/components/OurTeam/team.ts
@@ -29,7 +29,7 @@ export const team: TeamMember[] = [
     name: "Erisa Zaimi",
     role: "Frontend Engineer",
     funFact: "Typescript fanatic and Tailwind whisperer test test test test test test.",
-    photo: "/team/era.png",
+    photo: "/team/erap.png",
     social: {
       github: "https://github.com/ezaimi",
       linkedin: " https://www.linkedin.com/in/erisazaimi/ ",
