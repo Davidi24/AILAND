bffdc6e01f9956ab2f3913a50e3320a408246e88
David24 <dkeci21@epoka.edu.al>
2025-11-03 17:25:59 +0100

    test

--- website/src/components/OurTeam/team.ts
+++ website/src/components/OurTeam/team.ts
@@ -17,7 +17,7 @@ export const team: TeamMember[] = [
     id: "david",
     name: "David Keçi",
     role: "Full Stack Developer",
-    funFact: "I code by day and fix the bugs I created by night amd call it part of the plan.",
+    funFact: "I code by day and fix the bugs I created by night and call it part of the plan.",
     photo: "/team/david.jpg",
     social: {
       github: "https://github.com/Davidi24 ",
@@ -27,8 +27,8 @@ export const team: TeamMember[] = [
   {
     id: "era",
     name: "Erisa Zaimi",
-    role: "Frontend Engineer",
-    funFact: "Typescript fanatic and Tailwind whisperer test test test test test test.",
+    role: "UI/UX Designer & Frontend Developer",
+    funFact: "I turn “why doesn’t this work?” into “I meant to do that.”",
     photo: "/team/erap.png",
     social: {
       github: "https://github.com/ezaimi",
