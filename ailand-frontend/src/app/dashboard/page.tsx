e6c924db8ccd0d255e57e9c6c8f65798e8585465
David24 <dkeci21@epoka.edu.al>
2025-11-05 09:30:10 +0100

    Adding my first version of sidebar

---
+++ ailand-frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div className="flex flex-1   p-1 pt-0">
+    
+      <div className="flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    </div>
+  );
+}
