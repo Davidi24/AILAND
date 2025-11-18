5702b3821a71d5cb574b007c4f13b1cb0c68e9f0
Qasim Abdullah <ranaqasim739@yahoo.com>
2025-11-18 23:37:02 +0100

    fixing issues

--- ailand-backend/app/models/user.py
+++ ailand-backend/app/models/user.py
@@ -9,5 +9,3 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
