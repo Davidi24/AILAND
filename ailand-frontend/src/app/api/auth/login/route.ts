98526ba4d74637818c7b7ca1d9bacbe28b672edd
era-e <your-email@example.com>
2025-11-19 23:05:17 +0100

    adding login and register frontend

--- ailand-frontend/src/app/api/auth/login/route.ts
+++ ailand-frontend/src/app/api/auth/login/route.ts
@@ -1,43 +1,29 @@
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const { email, password } = body;
+"use server";
 
-    // Real backend (use later)
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify(body),
-    // });
-    // const data = await res.json();
-    // return new Response(JSON.stringify(data), {
-    //   status: res.status,
-    //   headers: { "Content-Type": "application/json" },
-    // });
+import { cookies } from "next/headers";
+import { loginUser } from "@/lib/api/auth"; 
 
-    // Mock backend (for now)
-    if (email === "test@ailand.com" && password === "123456") {
-      return new Response(
-        JSON.stringify({
-          token: "mock-token-12345",
-          user: { email, name: "Test User" },
-        }),
-        {
-          status: 200,
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-    } else {
-      return new Response(
-        JSON.stringify({ message: "Invalid credentials" }),
-        { status: 401 }
-      );
-    }
-  } catch (error) {
-    console.error("Mock login error:", error);
-    return new Response(
-      JSON.stringify({ message: "Internal server error" }),
-      { status: 500 }
-    );
-  }
+export async function loginAction(email: string, password: string) {
+  const data = await loginUser({ email, password }); 
+
+  (await
+    cookies()).set("auth_token", data.token, {
+    httpOnly: true,
+    secure: false,
+    path: "/",
+  });
+
+  (await
+    cookies()).set("user", JSON.stringify(data.user), {
+    httpOnly: false,
+    path: "/",
+  });
+
+  return data.user;
+}
+
+
+export async function logoutAction() {
+  (await cookies()).delete("auth_token");
+  (await cookies()).delete("user");
 }
--- ailand-frontend/src/app/dashboard/page.tsx
+++ ailand-frontend/src/app/dashboard/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
+import LogoutButton from "@/components/common/LogoutButton";
+import { AuthProvider } from "@/context/useAuth";
+
 export default function Home() {
+
   return (
-      <div className="flex flex-1   p-1 pt-0">
-    
+    <div className="flex flex-1   p-1 pt-0">
+      <AuthProvider>
+  <LogoutButton />
+      </AuthProvider>
+      
       <div className="flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
   );
