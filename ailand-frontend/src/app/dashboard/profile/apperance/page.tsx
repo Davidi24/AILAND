af0f43faf7025a2b6c139c17b3ace15b268762f7
Klea Haxhiu <klea.haxhiu@s2024.tu-chemnitz.de>
2025-12-13 21:58:23 +0100

    User Profile UI

---
+++ ailand-frontend/src/app/dashboard/profile/apperance/page.tsx
@@ -0,0 +1,39 @@
+// import { ContentSection } from '../components/content-section'
+// import { AppearanceForm } from './appearance-form'
+// import { FontProvider } from '@/context/font-provider'
+
+// function page() {
+//   return (
+//     <FontProvider>
+//       <ContentSection
+//         title="Appearance"
+//         desc="Customize the appearance of the app. Automatically switch between day and night themes."
+//       >
+//         <AppearanceForm />
+//       </ContentSection>
+//     </FontProvider>
+//   )
+// }
+
+// export default page
+import { ContentSection } from '../components/content-section'
+import { AppearanceForm } from './appearance-form'
+import { FontProvider } from '@/context/font-provider'
+import { ThemeProvider } from '@/context/theme-provider'
+
+function page() {
+  return (
+    <ThemeProvider>
+      <FontProvider>
+        <ContentSection
+          title="Appearance"
+          desc="Customize the appearance of the app. Automatically switch between day and night themes."
+        >
+          <AppearanceForm />
+        </ContentSection>
+      </FontProvider>
+    </ThemeProvider>
+  )
+}
+
+export default page
